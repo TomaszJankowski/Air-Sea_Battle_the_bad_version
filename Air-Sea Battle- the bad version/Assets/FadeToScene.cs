5b739b41a17fe21fbc65ffb4a748dbe06cde13ff
DESKTOP-HRPK8QB\User <tomaszadamjankowski@gmail.com>
2018-07-16 15:34:24 +0200

    no message

---
+++ Air-Sea Battle- the bad version/Assets/FadeToScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeToScene : MonoBehaviour {
+
+    [HideInInspector]
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("FadeInOptions");
+    }
+
+    private void OnDisable()
+    {
+        animator.SetTrigger("FadeOutOptions");
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
---
+++ Air-Sea Battle- the bad version/Assets/Scripts/Game/Enemy/DetectCol.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DetectCol : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            Physics2D.IgnoreCollision(col.collider, GetComponent<Collider2D>());
+        }
+    }
+}
